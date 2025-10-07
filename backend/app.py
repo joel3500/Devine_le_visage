4e7f6f9649f8dd4b0294e87b4659c8e447449ed8
joel3500 <114688793+joel3500@users.noreply.github.com>
2025-10-07 07:12:57 -0400

    Update app.py
    Update app.y to put it online.

--- backend/app.py
+++ backend/app.py
@@ -182,4 +182,5 @@ def health():
 # ========== Lancement local ==========
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, port=port)
+    #app.run(debug=True, port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)  # En prod (sur Render)
